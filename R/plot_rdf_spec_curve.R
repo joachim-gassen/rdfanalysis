88c48b3ade5839be463a59a6ec30cd6f83bb7723
Joachim Gassen <gassen@wiwi.hu-berlin.de>
2019-04-27 12:18:55 +0200

    Implemented Specification Curve Plot

--- R/plot_rdf_spec_curve.R
+++ R/plot_rdf_spec_curve.R
@@ -1,31 +1,48 @@
 #' Plot the Specification Curve of your Reseracher Degrees of Freedom
 #'
-#' Plots a visulization of the specification curve as introduced by
-#' Simonsohn, Simmons and Nelson.
+#' Visualizes a result estimate across your researcher desgrees of
+#' freedom by plotting its specification curve as introduced by
+#' \href{https://papers.ssrn.com/sol3/papers.cfm?abstract_id=2694998}{Simonsohn, Simmons and Nelson}.
 #'
-#' @param ests The data frame provided by \code{exhaust_design()}.
+#' @param ests The data frame provided by \code{\link[rdfanalysis:exhaust_design]{exhaust_design()}}.
 #' @param est A character string indicating the estimate that you want to plot.
 #' @param lb A character string indicating the lower bound of the estimate.
 #' @param ub A character string indicating the upper bound of the estimate.
-#' If both \code{lb} und \code{ub} are present the confindence interval around
-#' the estimate is plotted and the significane of each estimate is assessed.
+#' If both \code{lb} und \code{ub} are present the plot includes a confindence
+#' interval around the estimate and colors the estimate points according to
+#' their significance (see below).
 #' @param sample_frac The percentage of the protocols that should be plottted.
 #' If your design produces many degrees of freedom, sampling speeds up plotting
 #' time and makes the plot less clutered.
-#' @param sig_color Color of significant estimates (only used when \code{lb} and
-#' \code{ub} are present).
+#' @param est_color Color of estimates points for specification curve
+#' @param est_color_signeg Color of significantly negative estimates.
+#' @param est_color_sigpos Color of significantly positive estimates.
 #' @param ribbon_color Color of the confidence interval (only used when \code{lb} and
 #' \code{ub} are present).
-#' @return A plot.
-#' @details See the vignette of the package for further details.
+#' @param lower_to_upper The size of the choice part of the plot, relative to
+#' the specification curve itself.
+#' @return A ggplot object.
+#' @details
+#' Significance of estimates is only dislayed when \code{lb} and
+#' \code{ub} are present. It is assessed by testing whether
+#' \code{lb} and \code{ub} are of equal sign.
+#'
+#' See the vignette of the package for further details.
 #' @examples
 #' \dontrun{
 #'   print("Sorry. No examples yet.")
 #' }
 #' @export
 plot_rdf_spec_curve <- function(ests, est, lb = "", ub = "",
-                                     sample_frac = 0.1, sig_color = "red",
-                                     ribbon_color = "lightblue") {
+                                sample_frac = 0.1,
+                                est_color = "black",
+                                est_color_signeg = "yellow",
+                                est_color_sigpos = "blue",
+                                ribbon_color = "lightblue",
+                                lower_to_upper = 3) {
+  choice <- item <- n <- option <- sig <- NULL
+  # To make devtools::check() happy
+
   if (xor(lb == "", ub == "")) stop("lb and ub need to be provided both or not at all")
   choices <- names(ests)[attr(ests, "choices")]
   for(c in choices) {
@@ -41,12 +58,12 @@ plot_rdf_spec_curve <- function(ests, est, lb = "", ub = "",
       dplyr::select(!!! rlang::syms(choices),
              !! rlang::sym(est), !! rlang::sym(lb), !! rlang::sym(ub)) %>%
       dplyr::arrange(!! rlang::sym(est), !! rlang::sym(lb), !! rlang::sym(ub)) %>%
-      dplyr::mutate(n = row_number()) %>%
+      dplyr::mutate(n = dplyr::row_number()) %>%
       dplyr::sample_frac(sample_frac) %>%
-      dplyr::gather(key = "choice", value = "option", -n,
+      tidyr::gather(key = "choice", value = "option", -n,
              -!!rlang::sym(est), -!!rlang::sym(lb), -!!rlang::sym(ub)) %>%
       dplyr::mutate(item = sprintf("%s: %s", choice, option),
-             sig = !!rlang::sym(lb) > 0) %>%
+             sig = factor(sign(!!rlang::sym(lb)) + sign(!!rlang::sym(ub)))) %>%
       dplyr::select(n, choice, item, sig,
              !!rlang::sym(est), !!rlang::sym(lb), !!rlang::sym(ub)) %>%
       dplyr::arrange(n)
@@ -54,9 +71,9 @@ plot_rdf_spec_curve <- function(ests, est, lb = "", ub = "",
     es <- ests %>%
       dplyr::select(!!!rlang::syms(choices), !!rlang::sym(est)) %>%
       dplyr::arrange(!!rlang::sym(est)) %>%
-      dplyr::mutate(n = row_number()) %>%
+      dplyr::mutate(n = dplyr::row_number()) %>%
       dplyr::sample_frac(sample_frac) %>%
-      dplyr::gather(key = "choice", value = "option", -n,
+      tidyr::gather(key = "choice", value = "option", -n,
              -!!rlang::sym(est)) %>%
       dplyr::mutate(item = sprintf("%s: %s", choice, option)) %>%
       dplyr::select(n, choice, item, !!rlang::sym(est)) %>%
@@ -71,32 +88,35 @@ plot_rdf_spec_curve <- function(ests, est, lb = "", ub = "",
       dplyr::select(n, sig,
              !!rlang::sym(est), !!rlang::sym(lb), !!rlang::sym(ub)) %>%
       dplyr::distinct() %>%
-      ggplot2::ggplot(aes(x = n, y = !!rlang::sym(est))) +
-      ggplot2::geom_ribbon(aes(ymin = !!rlang::sym(lb), ymax = !!rlang::sym(ub)),
-                  fill = ribbon_color, color = ribbon_color) +
-      ggplot2::geom_point(aes(color = sig), size = 0.01) +
+      ggplot2::ggplot(ggplot2::aes(x = n, y = !!rlang::sym(est))) +
+      ggplot2::geom_ribbon(ggplot2::aes(ymin = !!rlang::sym(lb),
+                                        ymax = !!rlang::sym(ub)),
+                           fill = ribbon_color, color = ribbon_color) +
+      ggplot2::geom_point(ggplot2::aes(color = sig), size = 0.01) +
       ggplot2::theme_minimal() +
-      ggplot2::theme(axis.title.x=element_blank(),
-            axis.text.x=element_blank(),
-            axis.ticks.x=element_blank()) +
+      ggplot2::theme(axis.title.x = ggplot2::element_blank(),
+            axis.text.x = ggplot2::element_blank(),
+            axis.ticks.x = ggplot2::element_blank()) +
       ggplot2::guides(color = FALSE) +
-      ggplot2::scale_color_manual(values=c("black", sig_color))
+      ggplot2::scale_color_manual(values = c("-2" = est_color_signeg,
+                                             "0" = est_color,
+                                             "2" = est_color_sigpos))
   } else {
     sc <- es %>%
       dplyr::select(n, !!rlang::sym(est)) %>%
       dplyr::distinct() %>%
-      ggplot2::ggplot(aes(x = n, y = !!rlang::sym(est))) +
-      ggplot2::geom_point(size = 0.01) +
+      ggplot2::ggplot(ggplot2::aes(x = n, y = !!rlang::sym(est))) +
+      ggplot2::geom_point(size = 0.01, color = est_color) +
       ggplot2::theme_minimal() +
-      ggplot2::theme(axis.title.x=element_blank(),
-            axis.text.x=element_blank(),
-            axis.ticks.x=element_blank())
+      ggplot2::theme(axis.title.x = ggplot2::element_blank(),
+            axis.text.x = ggplot2::element_blank(),
+            axis.ticks.x = ggplot2::element_blank())
   }
 
-  dc <- ggplot2::ggplot(data = es, aes(x = n, y = item, color = choice)) +
+  dc <- ggplot2::ggplot(data = es, ggplot2::aes(x = n, y = item, color = choice)) +
     ggplot2::geom_point(size = 0.01) +
     ggplot2::theme_minimal() +
-    ggplot2::xlab("Protocol ID") +
+    ggplot2::xlab("Protocol") +
     ggplot2::ylab("") +
     ggplot2::guides(color = FALSE)
 
@@ -104,9 +124,22 @@ plot_rdf_spec_curve <- function(ests, est, lb = "", ub = "",
   gdc <- ggplot2::ggplot_build(dc)
   gA <- ggplot2::ggplot_gtable(gsc)
   gB <- ggplot2::ggplot_gtable(gdc)
-  g <- gtable:::rbind_gtable(gA, gB, "last")
+
+  # The following is more or less c&p from
+  # g <- gtable:::rbind_gtable(gA, gB, "last")
+  # to avoid the devtools::check note
+  # complaining about the tripple `:::`
+  gB$layout$t <- gB$layout$t + nrow(gA)
+  gB$layout$b <- gB$layout$b + nrow(gA)
+  gA$layout <- rbind(gA$layout, gB$layout)
+  gA$heights <- grid::unit.c(gA$heights, gB$heights)
+  gA$rownames <- c(gA$rownames, gB$rownames)
+  gA$widths <- gB$widths
+  gA$grobs <- append(gA$grobs, gB$grobs)
+  g <- gA
+
   panels <- g$layout$t[grep("panel", g$layout$name)]
-  g$heights[panels[2]] <- unit(3,"null")
+  g$heights[panels[2]] <- ggplot2::unit(lower_to_upper,"null")
   grid::grid.newpage()
   grid::grid.draw(g)
 }
